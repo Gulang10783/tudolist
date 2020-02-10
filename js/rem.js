122dcf3bb6072e4e98175eba6833e5f95b243aaa
Gulang10783 <57823185+Gulang10783@users.noreply.github.com>
2020-02-10 16:26:55 +0800

    Add files via upload

---
+++ js/rem.js
@@ -0,0 +1,7 @@
+	(function(){ screenWidth = window.innerWidth / 3.75
+	document.querySelector('html').style.fontSize = screenWidth + 'px'
+	window.onresize = function() {
+		console.log(window.innerWidth)
+		var screenWidth = window.innerWidth / 3.75
+		document.querySelector('html').style.fontSize = screenWidth + 'px'
+	}})()
